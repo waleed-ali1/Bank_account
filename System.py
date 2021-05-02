44d21e4cd9a0fd2a6187698fce9b07c64e588a78
walid <walid@sever.local>
2021-05-02 15:35:58 +0300

    initializing a dictionary contain Client infos with menu option

---
+++ System.py
@@ -0,0 +1,32 @@
+data = [
+    {'ID' : 123, 'Password' : 'WalidAli1' , 'name':'Walid Ali' , 'AccountBalance': 1500},
+    {'ID' : 200, 'Password' : 'ZiadAli1', 'name': 'Ziad Ali' , 'AccountBalance': 2000}
+]
+
+
+
+
+
+
+
+
+menu_option = """
+====================
+1. Login as Client
+2. Login as Admin
+3. Quit
+
+What would you like to do? """
+
+program_is_running = True
+
+while program_is_running:
+    decision = int(input(menu_option))
+    if decision == 1:
+        add_Client()
+        
+    elif decision == 3:
+        program_is_running = False
+    else:
+        print()
+        print('please choose a valid option')    
